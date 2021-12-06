c264ac8094a93f8b1224bdbcdc1ea2b52e217330
Fareed Tahir <76517762+fareedTahir131@users.noreply.github.com>
2021-12-06 20:45:08 +0500

    Short Audio Playing

---
+++ Assets/Scripts/ShortAudioPlayer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using RenderHeads.Media.AVProVideo;
+using UnityEngine;
+
+public class ShortAudioPlayer : MonoBehaviour
+{
+
+   
+
+    private MediaPlayer mediaPlayer;
+
+    private MediaPlayer.FileLocation location = MediaPlayer.FileLocation.RelativeToStreamingAssetsFolder;
+    private string folder = "wordbank/short/";
+    private string fileName;
+
+
+    private void Start()
+    {
+		mediaPlayer = GetComponent<MediaPlayer>();
+		mediaPlayer.Events.AddListener(OnVideoEvent);
+	}
+
+
+    public void PlayShortAudio()
+    {
+        fileName = Random.Range(0, 5034).ToString() + ".mp3";
+		mediaPlayer.OpenVideoFromFile(location, folder + fileName);
+	}
+
+	public void OnVideoEvent(MediaPlayer mp, MediaPlayerEvent.EventType et, ErrorCode errorCode)
+	{
+		switch (et)
+		{
+			case MediaPlayerEvent.EventType.ReadyToPlay:
+				break;
+			case MediaPlayerEvent.EventType.Started:
+				break;
+			case MediaPlayerEvent.EventType.FirstFrameReady:
+				break;
+			case MediaPlayerEvent.EventType.FinishedPlaying:
+				PlayShortAudio();
+				break;
+		}
+
+		Debug.Log("Event: " + et.ToString());
+	}
+
+}
